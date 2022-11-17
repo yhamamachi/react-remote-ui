b4397caeed3a7b610ca1e99285b32043c1fb2584
Yuya Hamamachi <94039217+yhamamachi@users.noreply.github.com>
2022-11-17 14:05:58 +0900

    Update SampleWindow

--- src/Window/SampleWindow.js
+++ src/Window/SampleWindow.js
@@ -5,9 +5,38 @@ import { styles } from "../theme";
 export const SampleWindow = () => {
   return (
     <div style={styles.pane}>
-      <object type="text/html" data="/dummy.html" width="100%" height="100%">
-        <p>Note: It seems that your browser doesn't support Object tag.</p>
-      </object>
+      <div>
+        <p style={{ buttom: "0" }}>
+          <button onClick={button_a}>aaaa</button>
+          <button onClick={button_b}>bbbb</button>
+          <button onClick={button_c}>cccc</button>
+        </p>
+        <object
+          id="dummy_obj"
+          type="text/html"
+          data="/dummy.html"
+          width="100%"
+          height="100%"
+        >
+          <p>Note: It seems that your browser doesn't support Object tag.</p>
+        </object>
+      </div>
     </div>
   );
 };
+
+function button_a() {
+  var obj = document.getElementById("dummy_obj");
+  obj_data_non_query = obj.data.split("?")[0];
+  obj.data = obj_data_non_query + "?edgeIP=Button_A is Pressed";
+}
+function button_b() {
+  var obj = document.getElementById("dummy_obj");
+  obj_data_non_query = obj.data.split("?")[0];
+  obj.data = obj.data + "?edgeIP=Button_B is Pressed";
+}
+function button_c() {
+  var obj = document.getElementById("dummy_obj");
+  obj_data_non_query = obj.data.split("?")[0];
+  obj.data = obj.data + "?edgeIP=Button_C is Pressed";
+}
