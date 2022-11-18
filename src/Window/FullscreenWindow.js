5619f003f3352b25fb6d828391ae46ecda87a6aa
Yuya Hamamachi <94039217+yhamamachi@users.noreply.github.com>
2022-11-18 10:46:58 +0900

    Fix

--- src/Window/FullscreenWindow.js
+++ src/Window/FullscreenWindow.js
@@ -5,11 +5,9 @@ import { styles } from "../theme";
 export const FullscreenWindow = (URL: string) => {
   return (
     <div style={styles.pane}>
-      <div>
-        <object type="text/html" data="{URL}" width="100%" height="100%">
-          <p>Note: It seems that your browser doesn't support Object tag.</p>
-        </object>
-      </div>
+      <object type="text/html" data={URL} width="100%" height="100%">
+        <p>Note: It seems that your browser doesn't support Object tag.</p>
+      </object>
     </div>
   );
 };
